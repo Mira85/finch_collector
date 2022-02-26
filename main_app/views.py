f5e7e897efcfec9cfb89d32a05600c44cb8a5f8c
Mira Makadia <patelmirak@gmail.com>
2022-02-26 13:30:52 -0500

    WIP: adds toys for finch

--- main_app/views.py
+++ main_app/views.py
@@ -20,7 +20,14 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     feeding_form = FeedingForm()
-    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
+
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in =finch.toys.all().values_list('id'))
+
+    return render(request, 'finches/detail.html', {
+        'finch': finch, 
+        'feeding_form': feeding_form,
+        'toys': toys_finch_doesnt_have
+        })
 
 def add_feeding(request, finch_id):
     form = FeedingForm(request.POST)
@@ -31,6 +38,10 @@ def add_feeding(request, finch_id):
         new_feeding.finch_id = finch_id
         new_feeding.save()
     return redirect('detail', finch_id = finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
     model = Finch
